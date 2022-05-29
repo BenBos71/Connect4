39f74afa9041cfeedf61f6e3cdf057a996669920
BenBos <ben.bos1097@gmail.com>
2022-05-29 17:05:28 -0600

    Initial Commit: Basic Connect 4 Game

---
+++ ConnectFour/ConnectFour/BoardCell.swift
@@ -0,0 +1,13 @@
+//
+//  BoardCell.swift
+//  ConnectFour
+//
+//  Created by Ben Bos on 5/28/22.
+//
+
+import UIKit
+
+class BoardCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+}
